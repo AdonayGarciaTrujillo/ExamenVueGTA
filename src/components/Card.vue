747fb40ad174c4ed018bab2ce88b779805ef785c
AdonayGarciaTrujillo <a3521110522@alumno.uttehuacan.edu.mx>
2022-10-12 21:22:36 -0500

    ExamenGTA

---
+++ src/components/Card.vue
@@ -0,0 +1,57 @@
+<template>
+    
+    
+
+    <div>
+        <slot></slot> 
+                <p class="text-center"> {{ nombre }} </p>
+                <p class="text-center"> {{ descripsion }} </p>
+                <p class="text-center"> {{ precio }} </p>
+                <a href="https://www.facebook.com/" target="_blank">Comprar</a>
+    </div>
+    
+  
+</template>   
+
+
+
+
+<style scoped>
+   
+    
+    div{
+        width: 300px;
+        display: inline-block; 
+        text-align: center;
+        border: 1px solid black;
+        border-radius: 5px;
+        margin: 2%;
+        padding: 1%;
+      
+
+    }
+   
+    
+   
+</style>
+
+<script>
+
+    export default {
+        props: {
+        
+            nombre: {
+                String,
+            },
+            descripsion: {
+                String,
+            },
+            precio: {
+                String,
+            },
+        },
+
+
+    };
+</script>
+
